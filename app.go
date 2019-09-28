e7f821ec757fe10ac2168894dd1fb71c401b9d03
cocodrips <cocodrips@gmail.com>
2019-09-28 17:24:19 +0900

    今日の英語

---
+++ app.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/cocodrips/task-scraper/scraper"
+)
+
+func main() {
+	scraper.GetMessage(scraper.EnglishCondition, scraper.EnglishOutput)
+}
---
+++ scraper/scraper.go
@@ -0,0 +1,99 @@
+package scraper
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+)
+
+var (
+	max_depth int = 1000
+)
+
+type Message struct {
+	Ok       bool `json:"ok"`
+	Messages []struct {
+		Text      string `json:"text"`
+		User      string `json:"user"`
+		TimeStamp string `json:"ts"`
+	} `json:"messages"`
+	HasMore bool `json:"has_more"`
+}
+
+var (
+	endpoint = "https://slack.com/api/"
+)
+
+func channelHistoryUrl(timestamp string) (string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", err
+	}
+
+	u.Path = path.Join(u.Path, "channels.history")
+
+	q := u.Query()
+	q.Set("token", os.Getenv("OAUTH_TOKEN"))
+	q.Set("channel", os.Getenv("CHANNEL"))
+	q.Set("count", "1000")
+	if timestamp != "" {
+		q.Set("latest", timestamp)
+	}
+
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+}
+
+func EnglishCondition(message string) bool {
+	return strings.HasPrefix(message, "今日の英語")
+}
+
+func EnglishOutput(message string) {
+	for _, line := range strings.Split(message, "\n") {
+		if !strings.Contains(line, "今日の英語") {
+			fmt.Println(strings.Replace(line, "- ", "", -1))
+		}
+	}
+}
+
+func GetMessage(condition func(string) bool, output func(string)) {
+
+	var responseJson Message
+	timestamp := ""
+
+	for i := 0; i < max_depth; i++ {
+		// Create request
+		url, _ := channelHistoryUrl(timestamp)
+		req, _ := http.NewRequest("GET", url, nil)
+		res, _ := http.DefaultClient.Do(req)
+
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+
+		// Parse texta
+		err := json.Unmarshal(body, &responseJson)
+		if err != nil {
+			fmt.Printf("Failed to parse")
+		}
+		for _, message := range responseJson.Messages {
+			if message.User != os.Getenv("USER_ID") {
+				continue
+			}
+			if !condition(message.Text) {
+				continue
+			}
+
+			output(message.Text)
+			timestamp = message.TimeStamp
+		}
+		if !responseJson.HasMore {
+			break
+		}
+	}
+
+}
